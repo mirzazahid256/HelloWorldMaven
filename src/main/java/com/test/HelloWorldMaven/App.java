0c424442e01962f6c492dfbdd75deb886a6ff59b
mirzazahid256 <61903716+mirzazahid256@users.noreply.github.com>
2020-03-14 13:53:02 +0530

    Job change

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Testing my first Jenkins freestyle project!" );
+        System.out.println( "Testing my first job Jenkins freestyle project!" );
+        
     }
 }
